f0f6e32400464e791cc8950e88b38cda55d42ef6
inhota <julioeng2012@gmail.com>
2022-02-15 17:36:22 -0300

    Criar teste Hello World do conteudo S1.5, e verificar se o titulo esta presente na tela.

--- src/test/java/TreinamentoSelenium/AppTest.java
+++ src/test/java/TreinamentoSelenium/AppTest.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * Unit test for simple App.
@@ -35,6 +42,18 @@ public class AppTest
         Assert.assertEquals(10, soma);
         Assert.assertTrue(carro.equals("Gol"));
         Assert.assertTrue(cidade.equals("Belo Horizonte"));
+    }
+
+    @Test
+    public void helloworld(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("http://google.com");
+
+        Assert.assertEquals("Google", driver.getTitle());
+
+        driver.quit();
+
     }
 
 }
