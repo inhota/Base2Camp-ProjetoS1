793da3d834b46396f658de924cf5ecb64e6a93be
inhota <julioeng2012@gmail.com>
2022-02-15 15:42:44 -0300

    Criado variaveis globais e fericando a execução dos 3 teste

--- src/test/java/TreinamentoSelenium/AppTest.java
+++ src/test/java/TreinamentoSelenium/AppTest.java
@@ -10,19 +10,30 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+    String fruta = "Pera";
+    String empresa = "Base2";
+    String cidade = "Belo Horizonte";
+    String carro = "Gol";
+    int soma = 4+6;
 
     @Test
     public void meuPrimeiroTeste(){
-        String fruta = "Pera";
+
         Assert.assertTrue(fruta.equals("Pera"));
     }
 
     @Test
     public void meuSegundoTeste(){
-        String empresa = "Base2";
-        String cidade = "Belo Horizonte";
+
 
         Assert.assertTrue(empresa.equals("Base2"));
+        Assert.assertTrue(cidade.equals("Belo Horizonte"));
+    }
+
+    @Test
+    public void meuTerceiroTeste(){
+        Assert.assertEquals(10, soma);
+        Assert.assertTrue(carro.equals("Gol"));
         Assert.assertTrue(cidade.equals("Belo Horizonte"));
     }
 
